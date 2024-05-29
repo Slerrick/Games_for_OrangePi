bcc64a3a5e03efff34068df0a07528f7717ecd43
Slerrick <zaharegorbolobolov@gmail.com>
2024-05-29 22:23:05 +0300

    Создал генерацию примеров и правильный ответ. ОстаÐлось лишь дать кнопкам функции для счета ответов и сохранения в LocalStorage

--- Engine_Brain/Type.js
+++ Engine_Brain/Type.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     //INIT Html elements
-    var Box1 = document.getElementsByClassName("B1");
-    var Box2 = document.getElementsByClassName("B2");
-    var Box3 = document.getElementsByClassName("B3");
-    var example = document.getElementsByClassName("ex");
+    var Box1 = document.getElementById("B1");
+    var Box2 = document.getElementById("B2");
+    var Box3 = document.getElementById("B3");
+    var example = document.getElementById("ex");
+    var counter = document.getElementById("count");
     //Create function, that will be to make type of examples and true answer
     var Math_Operations_Array = ["-", "+", "/", "*"];
+    ///////////////////////////////////////////////////////////////////////
     var create_Examples = function (mArr) {
         var randOperation = mArr[Math.floor(Math.random() * mArr.length)];
         var firstNumber = Math.round(Math.random() * 100);
@@ -22,13 +24,74 @@ document.addEventListener("DOMContentLoaded", function () {
             secondNumber += 1;
         }
         ;
-        return "".concat(firstNumber).concat(randOperation).concat(secondNumber);
+        example.textContent = "".concat(firstNumber).concat(randOperation).concat(secondNumber);
+        return [randOperation, "".concat(firstNumber), "".concat(secondNumber)];
     };
-    //Tests
-    var i = 0;
-    while (i < 30) {
-        console.log(create_Examples(Math_Operations_Array));
-        i += 1;
-    }
+    ///////////////////////////////////////////////////////////////////////
+    //Create function, that will be to make 3 variables answer
+    var HTMLElements_BOXES = [Box1, Box2, Box3];
+    var Micro_Random = function () { return Math.round(Math.random()); };
+    ///////////////////////////////////////////////////////////////////////
+    var three_Var_for_Answer = function () {
+        var Array = create_Examples(Math_Operations_Array);
+        var First = Number(Array[1]);
+        var Second = Number(Array[2]);
+        var True_answer;
+        switch (Array[0]) {
+            case "/":
+                True_answer = First / Second;
+                break;
+            case "*":
+                True_answer = First * Second;
+                break;
+            case "-":
+                True_answer = First - Second;
+                break;
+            case "+":
+                True_answer = First + Second;
+                break;
+            default:
+                return [-404];
+        }
+        var Fake1 = Math.random() * 8;
+        var Fake2 = Math.random() * 15;
+        if (Micro_Random() === 0) {
+            First = Math.round((True_answer - Fake1) * 100) / 100;
+            Second = Math.round((True_answer + Fake2) * 100) / 100;
+        }
+        else {
+            First = Math.round((True_answer + Fake1) * 100) / 100;
+            Second = Math.round((True_answer - Fake2) * 100) / 100;
+        }
+        var randBox = HTMLElements_BOXES[Math.round(Math.random() * (HTMLElements_BOXES.length - 1))];
+        switch (randBox) {
+            case Box1:
+                Box1.textContent = "".concat(Math.round((True_answer * 100)) / 100);
+                Box2.textContent = "".concat(Math.round(Second * 100) / 100);
+                Box3.textContent = "".concat(Math.round(First * 100) / 100);
+                break;
+            case Box2:
+                Box2.textContent = "".concat(Math.round((True_answer * 100)) / 100);
+                Box3.textContent = "".concat(Math.round(Second * 100) / 100);
+                Box1.textContent = "".concat(Math.round(First * 100) / 100);
+                break;
+            case Box3:
+                Box3.textContent = "".concat(Math.round((True_answer * 100)) / 100);
+                Box1.textContent = "".concat(Math.round(Second * 100) / 100);
+                Box2.textContent = "".concat(Math.round(First * 100) / 100);
+                break;
+        }
+        return [True_answer];
+    };
+    ///////////////////////////////////////////////////////////////////////
+    //Infinity cycles 
+    three_Var_for_Answer();
+    var Speed_Of_Game = 3900;
+    var Counter = 0;
+    counter.textContent = "You score: 0";
+    var Infinity = setInterval(function () { return three_Var_for_Answer(); }, Speed_Of_Game);
+    //Create button function and counter
+    Box1.addEventListener("click", function () {
+    });
 });
 //END
--- Engine_Brain/Type.ts
+++ Engine_Brain/Type.ts
@@ -2,10 +2,11 @@ document.addEventListener("DOMContentLoaded", function(){
 
 //INIT Html elements
 
-const Box1 = document.getElementsByClassName("B1");
-const Box2 = document.getElementsByClassName("B2");
-const Box3 = document.getElementsByClassName("B3");
-const example = document.getElementsByClassName("ex");
+const Box1 = document.getElementById("B1") as HTMLElement;
+const Box2 = document.getElementById("B2") as HTMLElement;
+const Box3 = document.getElementById("B3") as HTMLElement;
+const example = document.getElementById("ex") as HTMLElement;
+const counter = document.getElementById("count") as HTMLElement;
 
 //Create function, that will be to make type of examples and true answer
 
@@ -13,7 +14,7 @@ const Math_Operations_Array: string[] = ["-","+","/","*"];
 
 
 ///////////////////////////////////////////////////////////////////////
-const create_Examples = (mArr: string[]): string =>{
+const create_Examples = (mArr: string[]): string[] =>{
 
     const randOperation = mArr[Math.floor(Math.random() * mArr.length)];
     let firstNumber: number = Math.round(Math.random() * 100);
@@ -23,20 +24,86 @@ const create_Examples = (mArr: string[]): string =>{
     if(secondNumber === 0){secondNumber += 1};
     if (!(secondNumber %2 === 0)){secondNumber += 1};
   
-    return `${firstNumber}${randOperation}${secondNumber}`;
+    example.textContent = `${firstNumber}${randOperation}${secondNumber}`;
+
+    return [randOperation,`${firstNumber}`,`${secondNumber}`];
   }
 ///////////////////////////////////////////////////////////////////////
 
 
 
+//Create function, that will be to make 3 variables answer
 
-//Tests
-let i = 0;
-while (i < 30) {
-    console.log(create_Examples(Math_Operations_Array));
-    i += 1;
+const HTMLElements_BOXES = [Box1,Box2,Box3];
+
+const Micro_Random = ():number =>{return Math.round(Math.random())}
+
+///////////////////////////////////////////////////////////////////////
+const three_Var_for_Answer = (): number[] =>{
+
+    const Array = create_Examples(Math_Operations_Array);
+    let First = Number(Array[1]);
+    let Second = Number(Array[2]);
+    let True_answer: number;
+    switch (Array[0]){
+        case "/":
+            True_answer = First / Second 
+        break;
+        case "*":
+            True_answer = First * Second 
+        break;
+        case "-":
+            True_answer = First - Second 
+        break;
+        case "+":
+            True_answer = First + Second 
+        break;
+        default:
+            return [-404]
+    }
+
+    const Fake1 = Math.random() * 8;
+    const Fake2 = Math.random() * 15;
+
+    if (Micro_Random() === 0){
+    First = Math.round((True_answer - Fake1) * 100) / 100;
+    Second = Math.round((True_answer + Fake2) * 100) / 100;
+    }else{
+    First = Math.round((True_answer + Fake1) * 100) / 100;
+    Second = Math.round((True_answer - Fake2) * 100) / 100;
+    }
+    const randBox = HTMLElements_BOXES[Math.round(Math.random() * (HTMLElements_BOXES.length - 1))];
+     
+    switch (randBox){
+        case Box1:
+            Box1.textContent = `${Math.round((True_answer * 100)) / 100}`; Box2.textContent = `${Math.round(Second * 100) / 100}`; Box3.textContent = `${Math.round(First * 100) / 100}`;
+            break;
+        case Box2:
+            Box2.textContent = `${Math.round((True_answer * 100)) / 100}`; Box3.textContent = `${Math.round(Second * 100) / 100}`; Box1.textContent = `${Math.round(First * 100) / 100}`;
+            break;
+        case Box3:
+            Box3.textContent = `${Math.round((True_answer * 100)) / 100}`; Box1.textContent = `${Math.round(Second * 100) / 100}`; Box2.textContent = `${Math.round(First * 100) / 100}`;
+            break;
+    }
+    return [True_answer]
 }
+///////////////////////////////////////////////////////////////////////
 
+//Infinity cycles 
+three_Var_for_Answer()
+
+let Speed_Of_Game: number = 3900;
+let Counter: number = 0;
+counter.textContent = "You score: 0"
+
+const Infinity = setInterval( () => three_Var_for_Answer(), Speed_Of_Game );
+  
+
+//Create button function and counter
+
+Box1.addEventListener("click", function(){
+
+})
 
 
 });
